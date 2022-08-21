a8585b7d8e1ab403b31a5f88ef6eb0b9dc78f8c3
karnell schultz <karnellschultz@gmail.com>
2022-08-21 16:10:11 +0200

    grid it up!

--- apps/game/pages/index.tsx
+++ apps/game/pages/index.tsx
@@ -10,16 +10,6 @@ import { countries } from "countries-list";
 
 const MAX_GUESSES = 6;
 
-const GameInformationContainer = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => (
-  <div className="flex justify-center items-center flex-col mx-2">
-    {children}
-  </div>
-);
-
 export default function Web() {
   const country = useCapitalGameStore(({ country }) => country);
   const hintCount = useCapitalGameStore(({ hintCount }) => hintCount);
@@ -58,50 +48,51 @@ export default function Web() {
   };
 
   return (
-    <div className="flex justify-center items-center flex-col mx-2 text-gray-700 ">
-      <Nav />
+    <div className="w-screen grid grid-cols-7 overflow-hidden">
+      <div className="col-start-2 col-span-5 md:col-start-3 md:col-span-3">
+        <Nav />
+        <div className="">
+          <h2 className="text-3xl my-4 text-center font-bold text-gray-700 uppercase">
+            {country.name}
+          </h2>
+          <h3 className="text-3xl text-center my-4">{country.emoji}</h3>
+        </div>
 
-      <GameInformationContainer>
-        <h2 className="text-3xl my-4 text-center font-bold text-gray-700 uppercase">
-          {country.name}
-        </h2>
-        <h3 className="text-3xl my-4">{country.emoji}</h3>
-      </GameInformationContainer>
+        <HintDetails />
+        <GuessGridContainer />
 
-      <HintDetails />
-      <GuessGridContainer />
-
-      {/* <form className="w-ful" onSubmit={handleGuessSubmit}> */}
-      <Select
-        ref={inputRef}
-        className="w-full appearance rounded
-            text-gray-700 my-1 leading-tight
-            focus:outline-none focus:shadow-outline"
-        searchable
-        placeholder="capital"
-        nothingFound="No capitals"
-        data={[...capitals]}
-        disabled={gameOver}
-      />
-      {/* </form> */}
-      <button
-        className="w-full rounded py-1 px-6 border-2
-          hover:bg-slate-50
-          disabled:bg-slate-300 disabled:cursor-not-allowed"
-        onClick={handleGuessClick}
-        disabled={gameOver}
-      >
-        Guess
-      </button>
-      <button
-        className="w-full rounded py-1 px-6 my-2 border-2 
-          hover:bg-slate-50
-          disabled:bg-slate-300 disabled:cursor-not-allowed"
-        onClick={incrementHintCount}
-        disabled={gameOver || hasHintsRemaining}
-      >
-        Hint
-      </button>
+        {/* <form className="w-ful" onSubmit={handleGuessSubmit}> */}
+        <Select
+          ref={inputRef}
+          className="w-full appearance rounded
+        text-gray-700 my-1 leading-tight
+        focus:outline-none focus:shadow-outline"
+          searchable
+          placeholder="capital"
+          nothingFound="No capitals"
+          data={[...capitals]}
+          disabled={gameOver}
+        />
+        {/* </form> */}
+        <button
+          className="w-full rounded py-1 px-6 border-2
+        hover:bg-slate-50
+        disabled:bg-slate-300 disabled:cursor-not-allowed"
+          onClick={handleGuessClick}
+          disabled={gameOver}
+        >
+          Guess
+        </button>
+        <button
+          className="w-full rounded py-1 px-6 my-2 border-2 
+        hover:bg-slate-50
+        disabled:bg-slate-300 disabled:cursor-not-allowed"
+          onClick={incrementHintCount}
+          disabled={gameOver || hasHintsRemaining}
+        >
+          Hint
+        </button>
+      </div>
     </div>
   );
 }
